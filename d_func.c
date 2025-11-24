f2153227cf4c69f1b3041ff24a5f3d20ea639338
root <root@ip-10-42-169-24.ec2.internal>
2025-11-24 14:52:41 +0000

    creation du d_fun

--- d_func.c
+++ d_func.c
@@ -2,5 +2,41 @@
 
 int d_func(va_list liste)
 {
+int n, count = 0;
+unsigned int num;
+char buffer[10];
+int ind = 0;
+unsigned int digit;
 
+
+n = va_arg(liste, int);
+if (n == 0)
+{
+_putchar ('0');
+return (1);
+}
+if (n < 0)
+{
+_putchar ('-');
+count++;
+num = -n;
+}
+else
+{
+num = n;
+}
+while (num > 0)
+{
+digit = num % 10;
+buffer[ind] = '0' + digit;
+ind++;
+num = num / 10;
+}
+while (ind > 0)
+{
+ind--;
+_putchar (buffer[ind]);
+count++;
+}
+return (count);
 }
