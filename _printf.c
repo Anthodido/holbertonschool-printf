6b7e6e76d4aba79e97d8b12de6f0055afc346047
Antoine <antoine.bats@gmail.com>
2025-11-24 10:06:19 +0000

    Ajout de la structure dans printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,13 @@ int _printf(const char *format, ...)
 va_list liste;
 unsigned int i;
 
+type_txt_t type[] = {
+	{"c", c_func},
+	{"s", s_func},
+	{"%", %_func},
+	{NULL, NULL},
+};
+
 va_start(list, format);
 
 for (i = 0; i < format; i++)
