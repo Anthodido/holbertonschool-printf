ed6af4a83b413297c480395538e35f76dfdf0ee5
Antoine <antoine.bats@gmail.com>
2025-11-24 10:05:24 +0000

    Ajout de c_func

--- c_func.c
+++ c_func.c
@@ -1,1 +1,17 @@
 #include "main.h"
+/**
+* c_func - Imprime un caractère passé en argument.
+* @liste: Liste d'arguments contenant le caractère à imprimer.
+*
+* Return: Le nombre de caractères imprimés (toujours 1).
+*/
+int c_func(va_list liste)
+{
+	char c;
+
+	c = va_arg(liste, int);
+
+	_putchar(c);
+
+	return(1);
+}
