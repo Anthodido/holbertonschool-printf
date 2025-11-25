700607954e0e0149cefccbcb2584de8a87303101
Antoine <antoine.bats@gmail.com>
2025-11-25 12:20:06 +0000

    Modif _printf.c Betty

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - Réimplémentation simplifiée de printf
  * @format: chaîne de format à analyser et à afficher
@@ -14,14 +13,14 @@ int _printf(const char *format, ...)
 {
 	int i, count = 0;
 format_func_t table[] = {
-    {'c', c_func},
-    {'s', s_func},
-    {'%', mod_func},
+	{'c', c_func},
+	{'s', s_func},
+	{'%', mod_func},
 	{'d', d_func},
 	{'i', d_func},
 	{'b', b_func},
 	{'u', u_func},
-    {'\0', NULL}
+	{'\0', NULL}
 };
 	va_list liste;
 
