7a4bab510be1c541a26d3cd1ff67320bdfc9333c
lucasscianna <sciannalucas0@gmail.com>
2025-11-25 14:11:43 +0000

    Ajout de la fonction X_func.c permettant d'afficher en hexadecimal en majuscule

---
+++ X_func.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * X_func - imprime un entier en hexadécimal majuscule
+ * @liste: liste d'arguments
+ * Return: nombre de caractères imprimés
+ */
+
+int X_func(va_list liste)
+{
+unsigned int n = va_arg(liste, unsigned int);
+char hex_digits[] = "0123456789ABCDEF";
+int count = 0;
+char buffer[20];
+int i = 0;
+
+if (n == 0)
+return (_putchar('0'));
+
+while (n > 0)
+{
+buffer[i++] = hex_digits[n % 16];
+n /= 16;
+}
+
+i--;
+while (i >= 0)
+count += _putchar(buffer[i--]);
+
+return (count);
+}
